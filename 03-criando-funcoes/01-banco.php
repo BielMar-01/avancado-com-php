3d2f38a20d4184e70752db90262060121274bbfa
Gabriel Marques <bielmarques922@gmail.com>
2022-05-12 11:04:13 -0300

    Criando funções - Criando if para dazer a função de sacar algum valor no saldo

---
+++ 03-criando-funcoes/01-banco.php
@@ -0,0 +1,28 @@
+<?php
+
+$contasCorrentes = [
+  '123.456.789-10' => [
+    'titular' => 'Gabriel',
+    'saldo' => 1000
+  ],
+  '123.456.789-11' => [
+    'titular' => 'Alberto',
+    'saldo' => 11000
+  ],
+  '123.456.789-12' => [
+    'titular' => 'Maria',
+    'saldo' => 100
+  ]
+];
+
+$contasCorrentes['123.456.789-10']['saldo'] -= 500;
+
+if (500 > $contasCorrentes['123.456.789-12']['saldo']) {
+  echo "Você não pode sacar este valor" . PHP_EOL;
+} else {
+  $contasCorrentes['123.456.789-12']['saldo'] -= 500;
+}
+
+foreach ($contasCorrentes as $cpf => $conta) {
+  echo $cpf . " " . $conta['titular'] . " " . $conta['saldo'] . PHP_EOL;
+}
