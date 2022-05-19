ba32e87a2d23d21a3ec98c6e0a8356d58d558190
Gabriel Marques <bielmarques922@gmail.com>
2022-05-19 12:05:30 -0300

    Referencias e mais listas - Valor vs Referencia

---
+++ 05-referencias-e-mais-listas/01-funcoes.php
@@ -0,0 +1,32 @@
+<?php
+
+function sacar(array $conta, float $valorSacar): array
+{
+  if ($valorSacar > $conta['saldo']) {
+    exibeMensagem("Você não pode sacar este valor");
+  } else {
+    $conta['saldo'] -= $valorSacar;
+  }
+  return $conta;
+}
+
+function exibeMensagem(string $mensagem)
+{
+  echo $mensagem . PHP_EOL;
+}
+
+function deposito(array $conta, float $valorADepositar): array
+{
+  if ($valorADepositar > 0) {
+    $conta['saldo'] += $valorADepositar;
+  } else {
+    exibeMensagem("Depositos precisam ser positivos");
+  }
+  return $conta;
+}
+
+function titularLetrasMaiscula(array &$conta)
+{
+  // $conta['titular'] = strtoupper($conta['titular']); // Já vem abilitado no php strtoupper
+  $conta['titular'] = mb_strtoupper($conta['titular']); // necessario abilitar o mbstring e o extension_dir no php.ini. Essa extensão trabalhar com strings mais complexas
+}
